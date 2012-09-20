a388aec1ca3107804f243e0c47b9a39065dfea01
Alexander Safronov <me@safron.su>
2012-09-21 02:32:10 +0400

    Record class: created_ad & updated_at autofilling fixed

--- src/RecordsMan/Record.php
+++ src/RecordsMan/Record.php
@@ -246,7 +246,7 @@ abstract class Record {
         if ($thisId) {
             // updating existing entry
             if ($this->hasOwnField('updated_at')) {
-                $this->updated_at = time();
+                $actualFields['updated_at'] = time();
             }
             $sqlParams = [];
             $sql = "UPDATE `{$tableName}` SET ";
@@ -261,7 +261,7 @@ abstract class Record {
         }
         // creating new entry
         if ($this->hasOwnField('created_at')) {
-            $this->created_at = time();
+            $actualFields['created_at'] = time();
         }
         self::getAdapter()->insert($tableName, $actualFields);
         $this->_fields['id'] = self::getAdapter()->getLastInsertId();
