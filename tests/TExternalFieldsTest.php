a948408ce4eee32be00494e9732f2bac3799ba94
Vladimir <skogx@list.ru>
2014-09-25 20:18:21 +0400

    Merge branch 'computed_fields' of https://github.com/Ekhvalov/recordsman
    Conflicts:
    	src/RecordsMan/TExternalFields.php
    	tests/test_items_setup.php

--- tests/TExternalFieldsTest.php
+++ tests/TExternalFieldsTest.php
@@ -8,23 +8,55 @@ class TExternalFieldsTest extends DBConnected_TestCase
 
     public function testAddExternalField()
     {
-        $this->assertClassHasStaticAttribute('externalFields', '\Test\ItemExt');
+        $this->assertClassHasStaticAttribute('_externalFields', '\Test\ItemExt');
         $itemExtReflection = new \ReflectionClass('\Test\ItemExt');
-        $tablesReflection = $itemExtReflection->getProperty('externalFields');
+        $tablesReflection = $itemExtReflection->getProperty('_externalFields');
         $tablesReflection->setAccessible(true);
         $this->assertTrue(is_array($tablesReflection->getValue()));
         $this->assertCount(0, $tablesReflection->getValue());
-        ItemExt::addExternalField('cityName', 'item_city');
-        $this->assertCount(1, $tablesReflection->getValue());
+        ItemExt::init();
+        $this->assertCount(2, $tablesReflection->getValue());
         $this->assertTrue(isset($tablesReflection->getValue()['cityName']));
         $this->assertEquals('item_city', $tablesReflection->getValue()['cityName']['table']);
-        $this->assertEquals('cityName', $tablesReflection->getValue()['cityName']['fieldKey']);
-        ItemExt::addExternalField('cityName', 'item_city', 'title');
-        $this->assertEquals('item_city', $tablesReflection->getValue()['cityName']['table']);
         $this->assertEquals('title', $tablesReflection->getValue()['cityName']['fieldKey']);
+    }
+
+    public function testAddExternalField_Get()
+    {
         $itemExt = ItemExt::load(1);
         $this->assertEquals('Saint-Petersburg', $itemExt->cityName);
+        $this->assertEquals('4.5', $itemExt->cityPopulation);
+        $itemExt = ItemExt::load(2);
+        $this->assertEquals('Moscow', $itemExt->cityName);
+        $this->assertEquals('11.5', $itemExt->cityPopulation);
     }
+
+    public function testAddExternalField_Set()
+    {
+        /** @var TExternalFields|ItemExt $itemExt */
+        $itemExt = ItemExt::load(1);
+        $this->assertEquals('Saint-Petersburg', $itemExt->cityName);
+        $this->assertEquals('4.5', $itemExt->cityPopulation);
+        $itemExt->setCityName('Leningrad');
+        $itemExt->setPopulation(4.8);
+        $this->assertEquals('Leningrad', $itemExt->cityName);
+        $this->assertEquals('4.8', $itemExt->cityPopulation);
+    }
+
+    public function testAddExternalField_Save()
+    {
+        /** @var TExternalFields|ItemExt $itemExt */
+        $itemExt = ItemExt::load(1);
+        $this->assertEquals('Saint-Petersburg', $itemExt->cityName);
+        $this->assertEquals('4.5', $itemExt->cityPopulation);
+        $itemExt->setCityName('Leningrad');
+        $itemExt->setPopulation(4.8);
+        $itemExt->saveExternalFields();
+        $itemExt = ItemExt::load(1);
+        $this->assertEquals('Leningrad', $itemExt->cityName);
+        $this->assertEquals('4.8', $itemExt->cityPopulation);
+    }
+
 
 }
 
