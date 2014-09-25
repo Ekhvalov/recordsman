16c638fab0fc020b8a9af2392beec0788ab64c82
Vladimir <skogx@list.ru>
2014-09-25 20:18:32 +0400

    Merge branch 'computed_fields' of https://github.com/Ekhvalov/recordsman
    Conflicts:
    	src/RecordsMan/TExternalFields.php
    	tests/test_items_setup.php

--- src/RecordsMan/TExternalFields.php
+++ src/RecordsMan/TExternalFields.php
@@ -3,24 +3,74 @@ namespace RecordsMan;
 
 trait TExternalFields
 {
-    protected static $externalFields = [];
+    private static $_externalFields = [];
+    private $_externalFieldsCache = [];
+    private $_externalFieldsChanged = [];
+    private $_parentId = 'parent_id';
 
     public static function addExternalField($fieldName, $tableName, $fieldKey = null) {
-        self::$externalFields[$fieldName]['table'] = $tableName;
-        self::$externalFields[$fieldName]['fieldKey'] = $fieldKey ?: $fieldName;
-        self::addProperty($fieldName, _createGetter(
-            self::$externalFields[$fieldName]['table'],
-            self::$externalFields[$fieldName]['fieldKey']
-        ));
+        self::$_externalFields[$fieldName]['table'] = $tableName;
+        self::$_externalFields[$fieldName]['fieldKey'] = $fieldKey ?: $fieldName;
+        self::addProperty($fieldName, _createGetter($fieldName), _createSetter($fieldName));
+    }
 
+    public function saveExternalFields() {
+        foreach ($this->_externalFieldsChanged as $tableName => $keysValues) {
+            $sql = $this->_getInsertSql($tableName, array_keys($keysValues));
+            /** @var Record|TExternalFields $this */
+            $params = [":{$this->_parentId}" => $this->id];
+            foreach ($keysValues as $placeholder => $value) {
+                $params[":{$placeholder}"] = $value;
+            }
+            Record::getAdapter()->query($sql, $params);
+            unset($this->_externalFieldsChanged[$tableName]);
+        }
+    }
+
+    private function _getInsertSql($tableName, $colNames) {
+        $keys = implode(',', array_map(function($colName) {
+            return "`{$colName}`";
+        }, $colNames));
+        $placeholders = implode(',', array_map(function($placeholder) {
+            return ":{$placeholder}";
+        }, $colNames));
+        $onDuplicate = implode(',', array_map(function($colName) {
+            return "`{$colName}`=:{$colName}";
+        }, $colNames));
+        $sql = "INSERT INTO `{$tableName}` (`{$this->_parentId}`,{$keys}) VALUES (:{$this->_parentId},{$placeholders}) ";
+        $sql.= "ON DUPLICATE KEY UPDATE {$onDuplicate};";
+        return $sql;
+    }
+
+    private function _getTableName($fieldName) {
+        return self::$_externalFields[$fieldName]['table'];
+    }
+
+    private function _getFieldKey($fieldName) {
+        return self::$_externalFields[$fieldName]['fieldKey'];
     }
 
 }
 
-function _createGetter($tableName, $fieldKey) {
-    return function() use ($tableName, $fieldKey) {
-        $sql = "SELECT `{$fieldKey}` FROM `{$tableName}` WHERE `parent_id`=?";
-        /** @var Record $this */
-        return Record::getAdapter()->fetchSingleValue($sql, [$this->id]);
+function _createGetter($fieldName) {
+    return function() use ($fieldName) {
+        /** @var Record|TExternalFields $this */
+        if (!isset($this->_externalFieldsCache[$fieldName])) {
+            $tableName = self::$_externalFields[$fieldName]['table'];
+            $fieldKey = self::$_externalFields[$fieldName]['fieldKey'];
+            $sql = "SELECT `{$fieldKey}` FROM `{$tableName}` WHERE `parent_id`=?";
+            $this->_externalFieldsCache[$fieldName] = Record::getAdapter()
+                ->fetchSingleValue($sql, [$this->id]);
+        }
+        return $this->_externalFieldsCache[$fieldName];
+    };
+}
+
+function _createSetter($fieldName) {
+    return function($value) use ($fieldName) {
+        /** @var $this Record|TExternalFields */
+        $this->_externalFieldsCache[$fieldName] = $value;
+        $this->_externalFieldsChanged[$this->_getTableName($fieldName)][$this->_getFieldKey($fieldName)] = $value;
+        return $value;
     };
 }
